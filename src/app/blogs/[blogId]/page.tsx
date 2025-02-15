1a9961689725e5440f08fee7d3101e7fa3425990
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-02-15 16:43:34 +0600

    feat: implement static params generation for blog details page and remove console log

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,5 +1,16 @@
 import BlogDetailsCard from '@/components/ui/BlogDetailsCard';
+import { IBlog } from '@/types';
 import React from 'react';
+
+export const generateStaticParams =async () => {
+    const res = await fetch('http://localhost:5000/blogs');
+    const blogs = await res.json();
+
+    return blogs.slice(0,3).map((blog: IBlog) => ({
+        blogId : blog.id
+    }))
+}
+
 
 const BlogDetailsPage = async ({
     params
