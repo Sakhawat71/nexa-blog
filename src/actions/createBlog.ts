479474da2f60788649c71683890ac2f159922163
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-02-15 17:20:45 +0600

    feat: implement blog creation form and server action for submitting data

---
+++ src/actions/createBlog.ts
@@ -0,0 +1,14 @@
+"use server"
+
+
+export const createBlog = async (data: FormData) => {
+    const res = await fetch('http://localhost:5000/blogs', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data),
+    });
+    const blogInfo = await res.json();
+    return blogInfo;
+};
---
+++ src/app/blogs/create/page.tsx
@@ -0,0 +1,14 @@
+import BlogForm from '@/components/ui/BlogForm';
+import React from 'react';
+
+const CreatePage = () => {
+
+
+    return (
+        <div>
+            <BlogForm />
+        </div>
+    );
+};
+
+export default CreatePage;
