cfffcc67a8ae200bb35a03209fb2c5435c6d3985
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-02-15 22:21:57 +0600

    feat: add base API slice for blog data fetching and integrate with Redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { baseApi } from './apis/blogs.slice'
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        [baseApi.reducerPath] : baseApi.reducer,
+    },
+
+    middleware : (GetDefaultMiddleware) => GetDefaultMiddleware().concat(baseApi.middleware),
 })
 
 // export type RootState = ReturnType<typeof store.getState>;
