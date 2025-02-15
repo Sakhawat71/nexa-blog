77dae6eba3554ca764d3992bca7e5a91bdab936a
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-02-15 17:45:38 +0600

    add generateMetadata function to fetch and return blog metadata

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -2,14 +2,27 @@ import BlogDetailsCard from '@/components/ui/BlogDetailsCard';
 import { IBlog } from '@/types';
 import React from 'react';
 
-export const generateStaticParams =async () => {
+export const generateStaticParams = async () => {
     const res = await fetch('http://localhost:5000/blogs');
     const blogs = await res.json();
 
-    return blogs.slice(0,3).map((blog: IBlog) => ({
-        blogId : blog.id
+    return blogs.slice(0, 3).map((blog: IBlog) => ({
+        blogId: blog.id
     }))
-}
+};
+
+
+export const generateMetadata = async ({ params }: { params: Promise<{ blogId: string }> }) => {
+
+    const { blogId } = await params;
+    const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
+    const blog = await res.json();
+
+    return {
+        title: blog.title,
+        description : blog.description,
+    }
+};
 
 
 const BlogDetailsPage = async ({
